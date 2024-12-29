ac118cf224183a279b84c9ec88e3a3ecd254fe70
Ashim <ashimsaha2311@gmail.com>
2024-12-29 13:19:23 +0530

    Fix deprecated code

--- lib/src/providers/share_pref_provider.dart
+++ lib/src/providers/share_pref_provider.dart
@@ -1,9 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 part 'share_pref_provider.g.dart';
 
 @riverpod
-Future<SharedPreferences> sharedPreferences(SharedPreferencesRef ref) async {
+Future<SharedPreferences> sharedPreferences(Ref ref) async {
   throw UnimplementedError();
 }
